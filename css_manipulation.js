1239ecae79b94358fa4e6fe638568f83843e7b04
Bea <beatriztattooer@gmail.com>
2020-12-01 13:37:31 -0300

    CSS MANIPULATION: clean code Use 'this' rather than use everytime the 'document.getElementByID'

--- css_manipulation.js
+++ css_manipulation.js
@@ -1,9 +1,9 @@
 // CSS Manipulation
+
 document.getElementById('btn_color').onclick = function(){
-
-    document.getElementById('btn_color').style.height = "100px"
-    document.getElementById('btn_color').style['background-color'] = "purple"
-    document.getElementById('btn_color').style['color'] = "white"
-    document.getElementById('btn_color').style.transform = "translateX(100px)"
-
+    // The element with id 'btn_color' is the same as 'this':
+    this.style.height = "100px"
+    this.style['background-color'] = "purple"
+    this.style['color'] = "white"
+    this.style.transform = "translateX(100px)"
 }
