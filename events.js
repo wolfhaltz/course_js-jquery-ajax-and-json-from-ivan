3fc44a644eb0f98f014b18568e97722cc92e88d8
Bea <beatriztattooer@gmail.com>
2020-12-01 13:28:57 -0300

    EVENTS: clean code Remove 'leave-me' btn and its function and, added some usefull comments

--- events.js
+++ events.js
@@ -1,19 +1,14 @@
-// // Get the buttons by ID:
-// document.getElementById('click-me').onclick = function(){
-//     console.log('Click me button!')
-// }
-
+// Get the button by ID:
 document.getElementById('hover-me').onmouseover = function(){
     console.log('You mouse over the button')
 }
-
-
-// // To check if some key was down:
-// document.onkeydown = function(){
-//     alert('You keydown ' + event.keyCode)
-// }
 
 // To do the same stuff of document.getElementById but using a function:
 function button_clicked(){
     alert('You clicked at button!')
 }
+
+// // To check if some key was down:
+// document.onkeydown = function(){
+//     alert('You keydown ' + event.keyCode)
+// }
