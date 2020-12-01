48de7b6111d75e3110ab27c404ec271af3d65fc4
Bea <beatriztattooer@gmail.com>
2020-12-01 15:01:00 -0300

    LOOPS IF, ELSE IF & ELSE: html & js files

---
+++ src/assets/js/24_conditionals_if_elseIf_else.js
@@ -0,0 +1,20 @@
+// CLASS 24: conditional if, else if and if
+
+var age = 17
+if(age < 18){
+    console.log('-18')
+}else if(age > 18){
+    console.log('+18')
+}else{
+    console.log('what are you doing?????')
+}
+
+// Use case:
+var grades = 8;
+var absences = 4;
+
+if(grades >= 7 && absences <= 4){
+    console.log('Approved!')
+}else{
+    console.log('Disapproved!')
+}
