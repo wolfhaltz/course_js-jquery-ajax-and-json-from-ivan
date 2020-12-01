bdff6db0cc7335f9faf556e9252a8e81b70a7904
Bea <beatriztattooer@gmail.com>
2020-12-01 13:15:08 -0300

    EVENTS: onmouseover when hover btn

--- events.js
+++ events.js
@@ -3,3 +3,6 @@ document.getElementById('click-me').onclick = function(){
     console.log('Click me button!')
     alert('You clicked at this button!')
 }
+document.getElementById('hover-me').onmouseover = function(){
+    alert('You mouse over the button')
+}
