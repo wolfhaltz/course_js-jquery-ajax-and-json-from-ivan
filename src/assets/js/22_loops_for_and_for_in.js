3d82cca38b98c76fcbf02c81bb95c7ceece741ee
Bea <beatriztattooer@gmail.com>
2020-12-01 14:10:32 -0300

    LOOPS FOR & FOR/IN: describe loop 'for'

---
+++ src/assets/js/22_loops_for_and_for_in.js
@@ -0,0 +1,12 @@
+// Class 22: loops for and for/in
+
+//The loop for:
+for(var a = 0; a < 5; a++){
+    console.log(a)
+}
+
+// Using loop for to use at an array:
+var students = ['Peter', 'Marie', 'Joseph']
+for(var b = 0; b < students.length; b++){
+    console.log(students[b])
+}
