9764f6afac9ca1db2139c01d39d6e533c8b2d7c7
Bea <beatriztattooer@gmail.com>
2020-12-01 14:38:43 -0300

    WHILE & DO/WHILE: text & js files

---
+++ src/assets/js/23_loops_while_and_do_while.js
@@ -0,0 +1,15 @@
+// CLASS 23: loops While and Do/While
+
+// While:
+var count = 0;
+while(count < 5){
+    console.log(count);
+    count++;
+}
+
+// Do/While:
+var count2 = 10;
+do{
+    console.log(count2);
+    count++;
+}while(count2 < 5);
